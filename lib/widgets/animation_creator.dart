6a178d8fb5be5aee49c38de492d66757d88c1981
Tóth Kristóf Imre <kristof01124@gmail.com>
2023-03-02 10:41:59 +0100

    Created more generic AnimationCreator classes

--- lib/widgets/animation_creator.dart
+++ lib/widgets/animation_creator.dart
@@ -1,21 +1,66 @@
-import 'dart:developer';
-
-import 'package:analyzer_plugin/utilities/pair.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart';
-import 'package:learning_dart/include/ArduinoNetwork/animation_message.dart';
 import 'package:learning_dart/include/ArduinoNetwork/message.dart';
 import 'package:learning_dart/widgets/stateful_slider.dart';
 
-/*  
-  The idea of this widget is to connect the UI and the messageData part of the code
-*/
+class ValueWithDefault<T> {
+  T value;
+  bool isDefault = false;
 
-/*
-  TODO: implement a ColorPicker and Slider class, that has the following functionality:
-    - Has a title by default
-    - Has some basic parameters set by default
-*/
+  ValueWithDefault(this.value);
+}
+
+class AnimationCreatorWrapper extends StatelessWidget {
+  final Widget child;
+  final ValueWithDefault value;
+
+  const AnimationCreatorWrapper(
+      {super.key, required this.child, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        child,
+        Checkbox(
+          value: value.isDefault,
+          onChanged: (checkboxValue) {
+            if (checkboxValue != null) {
+              value.isDefault = checkboxValue;
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class AnimationCreatorSlider extends StatelessWidget {
+  final double min, max;
+  final ValueWithDefault<double> currentValue;
+
+  const AnimationCreatorSlider({
+    super.key,
+    required this.min,
+    required this.max,
+    required this.currentValue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (currentValue.isDefault) {
+      return const SizedBox.shrink();
+    }
+    return AnimationCreatorWrapper(
+      value: currentValue,
+      child: StatefulSlider(
+        value: currentValue.value,
+        onChanged: (sliderValue) {
+          currentValue.value = sliderValue;
+        },
+      ),
+    );
+  }
+}
 
 class AnimationCreatorApplyButton extends StatelessWidget {
   final AnimationCreator creator;
@@ -51,54 +96,4 @@ abstract class AnimationCreator {
   AnimationCreator({this.onButtonPressed});
 }
 
-class FillEffectCreator extends AnimationCreator {
-  int? duration;
-  Color? color;
-  FillEffectCreator({this.duration, this.color, super.onButtonPressed});
-
-  @override
-  Widget build() {
-    List<Widget> children = [];
-    if (duration == null) {
-      duration = 0;
-      children.add(
-        StatefulSlider(
-          value: (duration as int).toDouble(),
-          onChanged: (value) {
-            duration = value.toInt();
-          },
-        ),
-      );
-    }
-    if (color == null) {
-      color = const Color.fromARGB(255, 0, 0, 0);
-      children.add(
-        ColorPicker(
-          enableAlpha: false,
-          labelTypes: const [],
-          pickerColor: (color as Color),
-          onColorChanged: (value) {
-            color = value;
-          },
-        ),
-      );
-    }
-    return Column(
-      children: children,
-    );
-  }
-
-  @override
-  Message send() {
-    if (duration == null || color == null) {
-      throw Exception(
-          'Tried to create message data to not fully initialized message.');
-    }
-    return FillEffectMessageBuilder(
-      FillEffectMessage(
-        (duration as int),
-        (color as Color),
-      ),
-    );
-  }
-}
+class FillEffectCreator extends AnimationCreator {}
